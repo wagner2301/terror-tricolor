98b64c446f254941b39428c5199602b3fefe4147
wagner2301 <151472675+wagner2301@users.noreply.github.com>
2026-07-21 03:09:28 -0300

    Update painel.js

--- commands/painel.js
+++ commands/painel.js
@@ -1,99 +1,93 @@
 const {
-
-SlashCommandBuilder,
 EmbedBuilder,
 ActionRowBuilder,
 ButtonBuilder,
-ButtonStyle
-
+ButtonStyle,
+StringSelectMenuBuilder
 } = require("discord.js");
-
-
-
-module.exports = {
-
-
-
-data: new SlashCommandBuilder()
-
-.setName("painel")
-
-.setDescription("Abrir painel de recrutamento Terror Tricolor"),
-
-
-
-
-async execute(interaction){
-
 
 
 const embed = new EmbedBuilder()
 
+.setColor("#E30613") // vermelho Terror Tricolor
 
-.setColor("#005CA9")
-
-
-.setTitle("🔵🔴⚪ Terror Tricolor | Recrutamento")
-
+.setTitle("🔴🔵⚪ TERROR TRICOLOR")
 
 .setDescription(`
 
+Bem-vindo ao sistema oficial de atendimento do **Terror Tricolor**.
 
-Bem-vindo ao sistema oficial de recrutamento.
-
-
-Clique no botão abaixo para iniciar seu recrutamento.
-
+Selecione abaixo o motivo do seu contato:
 
 🔴 **Recrutamento TUTT**
+> Abra um ticket para ser recrutado.
+
+🔵 **Suporte**
+> Problemas ou dúvidas.
+
+⚪ **Denúncias**
+> Envie uma denúncia.
+
+❓ **Dúvidas**
+> Tire suas dúvidas.
+
+**Terror Tricolor | Sistema de Tickets**
+
+`)
+
+.setImage("https://cdn.discordapp.com/attachments/1493905634360295504/1524518701116952677/ChatGPT_Image_8_07_2026_17_52_15.png?ex=6a5fdc2e&is=6a5e8aae&hm=fb8fdca70aeb0edf9bbd2dc04218a9b2f18d57028261c2216d5f04cffa5aad0b&");
 
 
-Nossa equipe entrará em contato com você.
+// MENU IGUAL O PRINT
 
-
-🔵 Terror Tricolor
-⚪ Atendimento Oficial
-
-
-`);
-
-
-
-const botoes = new ActionRowBuilder()
-
-
+const menu = new ActionRowBuilder()
 .addComponents(
 
+new StringSelectMenuBuilder()
 
-new ButtonBuilder()
+.setCustomId("ticket_menu")
 
-.setCustomId("recrutamento")
+.setPlaceholder("Escolha uma opção.")
 
-.setLabel("Recrutamento")
+.addOptions([
 
-.setEmoji("🔴")
+{
+label:"Recrutamento TUTT",
+description:"Abra para ser recrutado",
+value:"recrutamento",
+emoji:"🔴"
+},
 
-.setStyle(ButtonStyle.Danger)
+{
+label:"Suporte",
+description:"Ajuda com problemas",
+value:"suporte",
+emoji:"🔵"
+},
 
+{
+label:"Denúncias",
+description:"Faça uma denúncia",
+value:"denuncia",
+emoji:"⚪"
+},
+
+{
+label:"Dúvidas",
+description:"Tire suas dúvidas",
+value:"duvidas",
+emoji:"❓"
+}
+
+])
 
 );
 
 
-
-await interaction.reply({
-
+await channel.send({
 
 embeds:[embed],
 
-
-components:[botoes]
-
+components:[menu]
 
 });
-
-
-
-}
-
-
-};
