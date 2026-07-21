c128468167417aeab4ba799807de283c527cd8ee
wagner2301 <151472675+wagner2301@users.noreply.github.com>
2026-07-21 03:23:50 -0300

    Update painel.js

--- commands/painel.js
+++ commands/painel.js
@@ -8,94 +8,152 @@ StringSelectMenuBuilder
 
 module.exports = {
 
+
 data: new SlashCommandBuilder()
 
 .setName("painel")
 
-.setDescription("Painel de recrutamento"),
+.setDescription("Abrir painel de atendimento"),
 
 
 
 async execute(interaction){
 
 
+
 await interaction.deferReply({
+
 ephemeral:true
+
 });
 
 
 
 const embed = new EmbedBuilder()
 
+
+
 .setColor("#E30613")
 
-.setTitle("🔴🔵⚪ Terror Tricolor | Recrutamento")
+
+
+.setTitle("🔴🔵⚪ Centro de Atendimento")
+
+
 
 .setDescription(`
 
-Bem-vindo ao sistema oficial de recrutamento do **Terror Tricolor**.
+Olá! Bem-vindo(a) ao sistema de tickets.
+
+Para abrir um ticket, por favor clique no botão abaixo e nos informe o motivo do seu contato.
+
+Estamos aqui para ajudar!
 
 
-Selecione abaixo para iniciar seu atendimento.
+━━━━━━━━━━━━━━
 
 
-🔴 **Recrutamento TUTT**
-
-Nossa equipe entrará em contato com você.
+**Atendimento Via Ticket**
 
 
-🔵 Terror Tricolor
-⚪ Atendimento Oficial
+Após enviar o motivo, será gerado um canal de texto privado para que possamos ajudá-lo de forma segura e ágil.
+
+Estamos aqui para ajudar!
+
+
+🔴🔵⚪ Terror Tricolor
 
 `)
 
-.setImage("https://cdn.discordapp.com/attachments/1493905634360295504/1524518701116952677/ChatGPT_Image_8_07_2026_17_52_15.png");
+
+
+.setThumbnail(
+"https://cdn.discordapp.com/attachments/1525367337023311963/1528978418555813918/LOGO_2026.png"
+)
+
+
+
+.setImage(
+"https://cdn.discordapp.com/attachments/1493905634360295504/1524518701116952677/ChatGPT_Image_8_07_2026_17_52_15.png"
+);
+
 
 
 
 
 const menu = new ActionRowBuilder()
 
+
+
 .addComponents(
+
+
 
 new StringSelectMenuBuilder()
 
+
+
 .setCustomId("ticket_menu")
+
+
 
 .setPlaceholder("Escolha uma opção.")
 
-.addOptions(
+
+
+.addOptions([
+
+
 
 {
+
 label:"Recrutamento TUTT",
-description:"Abrir recrutamento",
+
+description:"Abra um ticket para ser recrutado",
+
 value:"recrutamento",
-emoji:"🔴"
+
+emoji:"<:logo:1468411463118098572>"
+
 }
 
-)
+
+
+])
+
+
 
 );
 
 
 
+
+
 await interaction.channel.send({
+
 
 embeds:[embed],
 
+
 components:[menu]
 
+
 });
+
 
 
 
 await interaction.editReply({
 
+
 content:"✅ Painel enviado!"
+
 
 });
 
 
+
 }
+
 
 };
