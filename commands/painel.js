757349e9e6a3b15b31c72cb1e1f154c0db096ac9
wagner2301 <151472675+wagner2301@users.noreply.github.com>
2026-07-21 02:00:17 -0300

    Update painel.js

--- commands/painel.js
+++ commands/painel.js
@@ -1,1 +1,72 @@
+const {
+  EmbedBuilder,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle
+} = require("discord.js");
 
+module.exports = {
+
+name: "painel",
+
+async execute(interaction){
+
+const embed = new EmbedBuilder()
+.setColor("#E30613")
+.setTitle("🏟️ TERRITÓRIO TRICOLOR")
+.setDescription(
+`
+Bem-vindo ao sistema oficial de atendimento do **Terror Tricolor**.
+
+Selecione abaixo o motivo do contato:
+
+🔴 Recrutamento TUTT
+🔵 Suporte
+⚪ Denúncias
+❓ Dúvidas
+`
+)
+.setFooter({
+text:"Terror Tricolor | Sistema de Tickets"
+});
+
+
+const botoes = new ActionRowBuilder()
+.addComponents(
+
+new ButtonBuilder()
+.setCustomId("recrutamento")
+.setLabel("Recrutamento")
+.setEmoji("🔴")
+.setStyle(ButtonStyle.Danger),
+
+new ButtonBuilder()
+.setCustomId("suporte")
+.setLabel("Suporte")
+.setEmoji("🔵")
+.setStyle(ButtonStyle.Primary),
+
+new ButtonBuilder()
+.setCustomId("denuncia")
+.setLabel("Denúncia")
+.setEmoji("⚪")
+.setStyle(ButtonStyle.Secondary),
+
+new ButtonBuilder()
+.setCustomId("duvida")
+.setLabel("Dúvidas")
+.setEmoji("❓")
+.setStyle(ButtonStyle.Success)
+
+);
+
+
+await interaction.reply({
+embeds:[embed],
+components:[botoes]
+});
+
+
+}
+
+}
