7f9cecec7dc53f661e0090bd1290717277577a89
wagner2301 <151472675+wagner2301@users.noreply.github.com>
2026-07-21 01:59:57 -0300

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,1 +1,33 @@
+const { REST, Routes } = require("discord.js");
+require("dotenv").config();
 
+const commands = [
+  {
+    name: "painel",
+    description: "Abre o painel de tickets do Terror Tricolor"
+  }
+];
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+
+    console.log("Registrando comandos...");
+
+    await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
+      {
+        body: commands
+      }
+    );
+
+    console.log("Comandos registrados!");
+
+  } catch (error) {
+    console.error(error);
+  }
+})();
